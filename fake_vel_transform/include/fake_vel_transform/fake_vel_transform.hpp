7ad72a27689a2ab85ec0e7b9a9c3e20bca969c65
LihanChen2004 <757003373@qq.com>
2025-03-02 15:55:40 +0800

    🐞 fix(fake_vel_transform): Add timestamp alignment
    Subscribe to local planner topic to synchronize velocity commands with odometry

--- fake_vel_transform/include/fake_vel_transform/fake_vel_transform.hpp
+++ fake_vel_transform/include/fake_vel_transform/fake_vel_transform.hpp
@@ -16,11 +16,16 @@
 #define FAKE_VEL_TRANSFORM__FAKE_VEL_TRANSFORM_HPP_
 
 #include <memory>
+#include <mutex>
 #include <string>
 
 #include "example_interfaces/msg/float32.hpp"
 #include "geometry_msgs/msg/twist.hpp"
+#include "message_filters/subscriber.h"
+#include "message_filters/sync_policies/approximate_time.h"
+#include "message_filters/synchronizer.h"
 #include "nav_msgs/msg/odometry.hpp"
+#include "nav_msgs/msg/path.hpp"
 #include "rclcpp/rclcpp.hpp"
 #include "tf2_ros/transform_broadcaster.h"
 
@@ -32,29 +37,39 @@ public:
   explicit FakeVelTransform(const rclcpp::NodeOptions & options);
 
 private:
-  void odomCallback(const nav_msgs::msg::Odometry::SharedPtr msg);
-
+  void syncCallback(
+    const nav_msgs::msg::Odometry::ConstSharedPtr & odom,
+    const nav_msgs::msg::Path::ConstSharedPtr & local_plan);
   void cmdVelCallback(const geometry_msgs::msg::Twist::SharedPtr msg);
-
   void cmdSpinCallback(example_interfaces::msg::Float32::SharedPtr msg);
+  void publishTransform();
 
   rclcpp::Subscription<geometry_msgs::msg::Twist>::SharedPtr cmd_vel_sub_;
-  rclcpp::Subscription<nav_msgs::msg::Odometry>::SharedPtr odom_sub_;
   rclcpp::Subscription<example_interfaces::msg::Float32>::SharedPtr cmd_spin_sub_;
+
+  message_filters::Subscriber<nav_msgs::msg::Odometry> odom_sub_filter_;
+  message_filters::Subscriber<nav_msgs::msg::Path> local_plan_sub_filter_;
+  using SyncPolicy =
+    message_filters::sync_policies::ApproximateTime<nav_msgs::msg::Odometry, nav_msgs::msg::Path>;
+  std::unique_ptr<message_filters::Synchronizer<SyncPolicy>> sync_;
 
   rclcpp::Publisher<geometry_msgs::msg::Twist>::SharedPtr cmd_vel_chassis_pub_;
 
-  // Broadcast tf from robot_base to robot_base_fake
   std::unique_ptr<tf2_ros::TransformBroadcaster> tf_broadcaster_;
+
+  rclcpp::TimerBase::SharedPtr timer_;
 
   std::string robot_base_frame_;
   std::string fake_robot_base_frame_;
   std::string odom_topic_;
+  std::string local_plan_topic_;
   std::string cmd_spin_topic_;
   std::string input_cmd_vel_topic_;
   std::string output_cmd_vel_topic_;
   float spin_speed_;
 
+  std::mutex cmd_vel_mutex_;
+  geometry_msgs::msg::Twist::SharedPtr latest_cmd_vel_;
   double current_robot_base_angle_;
 };
 
--- fake_vel_transform/src/fake_vel_transform.cpp
+++ fake_vel_transform/src/fake_vel_transform.cpp
@@ -14,7 +14,7 @@
 
 #include "fake_vel_transform/fake_vel_transform.hpp"
 
-#include "tf2/utils.h"
+#include "tf2/utils.hpp"
 #include "tf2_geometry_msgs/tf2_geometry_msgs.hpp"
 
 namespace fake_vel_transform
@@ -27,14 +27,16 @@ FakeVelTransform::FakeVelTransform(const rclcpp::NodeOptions & options)
   this->declare_parameter<std::string>("robot_base_frame", "gimbal_link");
   this->declare_parameter<std::string>("fake_robot_base_frame", "gimbal_link_fake");
   this->declare_parameter<std::string>("odom_topic", "odom");
+  this->declare_parameter<std::string>("local_plan_topic", "local_plan");
   this->declare_parameter<std::string>("cmd_spin_topic", "cmd_spin");
   this->declare_parameter<std::string>("input_cmd_vel_topic", "");
   this->declare_parameter<std::string>("output_cmd_vel_topic", "");
   this->declare_parameter<float>("init_spin_speed", 0.0);
 
   this->get_parameter("robot_base_frame", robot_base_frame_);
-  this->get_parameter("odom_topic", odom_topic_);
   this->get_parameter("fake_robot_base_frame", fake_robot_base_frame_);
+  this->get_parameter("odom_topic", odom_topic_);
+  this->get_parameter("local_plan_topic", local_plan_topic_);
   this->get_parameter("cmd_spin_topic", cmd_spin_topic_);
   this->get_parameter("input_cmd_vel_topic", input_cmd_vel_topic_);
   this->get_parameter("output_cmd_vel_topic", output_cmd_vel_topic_);
@@ -48,24 +50,22 @@ FakeVelTransform::FakeVelTransform(const rclcpp::NodeOptions & options)
   cmd_spin_sub_ = this->create_subscription<example_interfaces::msg::Float32>(
     cmd_spin_topic_, 1, std::bind(&FakeVelTransform::cmdSpinCallback, this, std::placeholders::_1));
   cmd_vel_sub_ = this->create_subscription<geometry_msgs::msg::Twist>(
-    input_cmd_vel_topic_, 1,
+    input_cmd_vel_topic_, 10,
     std::bind(&FakeVelTransform::cmdVelCallback, this, std::placeholders::_1));
-  odom_sub_ = this->create_subscription<nav_msgs::msg::Odometry>(
-    odom_topic_, 10, std::bind(&FakeVelTransform::odomCallback, this, std::placeholders::_1));
-}
 
-void FakeVelTransform::odomCallback(const nav_msgs::msg::Odometry::SharedPtr msg)
-{
-  current_robot_base_angle_ = tf2::getYaw(msg->pose.pose.orientation);
+  // In Navigation2 Humble release, the velocity is published by the controller without timestamped.
+  // We consider the velocity is published at the same time as local_plan.
+  // Therefore, we use ApproximateTime policy to synchronize `cmd_vel` and `odometry`.
+  odom_sub_filter_.subscribe(this, odom_topic_);
+  local_plan_sub_filter_.subscribe(this, local_plan_topic_);
+  sync_ = std::make_unique<message_filters::Synchronizer<SyncPolicy>>(
+    SyncPolicy(100), odom_sub_filter_, local_plan_sub_filter_);
+  sync_->registerCallback(
+    std::bind(&FakeVelTransform::syncCallback, this, std::placeholders::_1, std::placeholders::_2));
 
-  geometry_msgs::msg::TransformStamped t;
-  t.header.stamp = msg->header.stamp;
-  t.header.frame_id = robot_base_frame_;
-  t.child_frame_id = fake_robot_base_frame_;
-  tf2::Quaternion q;
-  q.setRPY(0, 0, -current_robot_base_angle_);
-  t.transform.rotation = tf2::toMsg(q);
-  tf_broadcaster_->sendTransform(t);
+  // Timer to send transform from `robot_base_frame` to `fake_robot_base_frame`
+  timer_ = this->create_wall_timer(  // 20 Hz
+    std::chrono::milliseconds(50), std::bind(&FakeVelTransform::publishTransform, this));
 }
 
 void FakeVelTransform::cmdSpinCallback(const example_interfaces::msg::Float32::SharedPtr msg)
@@ -73,17 +73,56 @@ void FakeVelTransform::cmdSpinCallback(const example_interfaces::msg::Float32::S
   spin_speed_ = msg->data;
 }
 
-// Transform the velocity from `robot_base_frame` to `fake_robot_base_frame`
 void FakeVelTransform::cmdVelCallback(const geometry_msgs::msg::Twist::SharedPtr msg)
 {
+  std::lock_guard<std::mutex> lock(cmd_vel_mutex_);
+
+  if (msg->linear.x == 0.0f && msg->linear.y == 0.0f && msg->angular.z == 0.0f) {
+    geometry_msgs::msg::Twist temp_cmd = *msg;
+    temp_cmd.angular.z = spin_speed_;
+    cmd_vel_chassis_pub_->publish(temp_cmd);
+  } else {
+    latest_cmd_vel_ = msg;
+  }
+}
+
+void FakeVelTransform::syncCallback(
+  const nav_msgs::msg::Odometry::ConstSharedPtr & odom_msg,
+  const nav_msgs::msg::Path::ConstSharedPtr & /*local_plan_msg*/)
+{
+  geometry_msgs::msg::Twist::SharedPtr current_cmd_vel;
+  {
+    std::lock_guard<std::mutex> lock(cmd_vel_mutex_);
+    if (!latest_cmd_vel_) {
+      return;
+    }
+    current_cmd_vel = latest_cmd_vel_;
+  }
+
+  current_robot_base_angle_ = tf2::getYaw(odom_msg->pose.pose.orientation);
+
   float angle_diff = current_robot_base_angle_;
 
   geometry_msgs::msg::Twist aft_tf_vel;
-  aft_tf_vel.angular.z = msg->angular.z + spin_speed_;
-  aft_tf_vel.linear.x = msg->linear.x * cos(angle_diff) + msg->linear.y * sin(angle_diff);
-  aft_tf_vel.linear.y = -msg->linear.x * sin(angle_diff) + msg->linear.y * cos(angle_diff);
+  aft_tf_vel.angular.z = current_cmd_vel->angular.z + spin_speed_;
+  aft_tf_vel.linear.x =
+    current_cmd_vel->linear.x * cos(angle_diff) + current_cmd_vel->linear.y * sin(angle_diff);
+  aft_tf_vel.linear.y =
+    -current_cmd_vel->linear.x * sin(angle_diff) + current_cmd_vel->linear.y * cos(angle_diff);
 
   cmd_vel_chassis_pub_->publish(aft_tf_vel);
+}
+
+void FakeVelTransform::publishTransform()
+{
+  geometry_msgs::msg::TransformStamped t;
+  t.header.stamp = this->get_clock()->now();
+  t.header.frame_id = robot_base_frame_;
+  t.child_frame_id = fake_robot_base_frame_;
+  tf2::Quaternion q;
+  q.setRPY(0, 0, -current_robot_base_angle_);
+  t.transform.rotation = tf2::toMsg(q);
+  tf_broadcaster_->sendTransform(t);
 }
 
 }  // namespace fake_vel_transform
